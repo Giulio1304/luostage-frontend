a75fb2a4f656f2e64cf8033c67ce75d9d9cc98de
Giulio1304 <giuliotricinelli@yahoo.it>
2026-07-07 16:39:49 +0200

    Implementazione insert

--- src/app/pages/product-list/product-list.ts
+++ src/app/pages/product-list/product-list.ts
@@ -1,36 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Product } from '../../core/models/product';
+
+import { CommonModule, NgFor } from '@angular/common';
+import { Router } from '@angular/router';
 import { ProductService } from '../../core/services/productService';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   imports: [CommonModule],
+  standalone: true,
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
-export class ProductList implements OnInit{
-   products : Product[] =[];
-   error: string  | null=null;
+export class ProductList implements OnInit {
+  products: Product[] = [];
+  error: string | null = null;
 
-  constructor(private productService: ProductService){
+  constructor(private productService: ProductService, private router: Router, private cdr: ChangeDetectorRef) {
 
   }
 
   ngOnInit(): void {
+    console.log("NGONINIT");
     this.productService.getAllProds()
-    .subscribe({
-      next:(response) => {
-        this.products = response;
-      }, 
-      error: (err) => {
-        console.log(err);
-        this.error='errore nel caricamento dei prodotti!'
+      .subscribe({
+        next: (response) => {
+          this.products = response;
+          this.cdr.detectChanges();
+        },
+        error: (err) => {
+          console.log(err);
+          this.error = 'errore nel caricamento dei prodotti!'
 
-      }
-    });
-    
+        }
+      });
+  }
 
+  goToNewProduct(): void {
+    this.router.navigate(["/product-new"]);
   }
 
 
